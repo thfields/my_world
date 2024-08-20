189a668285c9a7340de19b180ae4ad8b2e76d2b9
thfields <thiagoabcfc@hotmail.com>
2024-08-20 19:12:59 -0300

    refactor: atualizando aboutpage

--- src/components/Aboutpage.jsx
+++ src/components/Aboutpage.jsx
@@ -1,36 +1,30 @@
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
 import { skillsData } from '../data/SkillsData';
 import { skillsImage } from '../utils/SkillsImage';
 import "../pages/style.css";
 
-
 function Aboutpage() {
     return (
         <div className='aboutpagebackground'>
             <Container>
-                    
                 <Row className='textbackground'>
-                    
-                    <Col md={7} >
+                    <Col md={7}>
                         <h3 className='aboutmetext'>Sobre <span>Mim</span></h3>
-                        <p className='aboutdetails'> 
-                        <p>Sou estudante, cursando a graduação de programação no IFRN, <br/>
-                            tenho 26 anos e experiência profissional em redes e manutenção de computadores, banco de dados e sistemas de help desk.
-                            <br/>
-                            <br/>
-                            Sou apaixonado por conhecimento e me encontrei na área de desenvolvimento de sistemas,
-                            pois gosto de desvendar desafios e tenho uma curiosidade que me move a aprender constantemente.
-                            <br/>
-                            <br/>
-                            Acho fascinante o avanço da tecnologia e as novidades que surgem diariamente,
-                            então, me sinto no dever de participar e colaborar com esses avanços.
-                            <br/> 
-                            <br/> 
-                            Estou a procura de uma oportunidade para aprender,<br/> 
-                            desenvolver e contribuir com crescimento da equipe
-                            colocando em prática o que estou estudando todos os dias.
-                            </p>
+                        <p className='aboutdetails'>
+                            Sou graduando no tecnólogo do IFRN e atuo como desenvolvedor full stack na própria instituição.
+                        </p>
+                        <p className='aboutdetails'>
+                            Além disso, possuo experiência profissional em banco de dados, manutenção e redes.
+                        </p>
+                        <p className='aboutdetails'>
+                            Tenho 27 anos e sou apaixonado por conhecimento. Me encontrei na área de desenvolvimento de sistemas, pois gosto de desvendar desafios e tenho uma curiosidade que me move a aprender constantemente.
+                        </p>
+                        <p className='aboutdetails'>
+                            Acho fascinante o avanço da tecnologia e as novidades que surgem diariamente, então, me sinto no dever de participar e colaborar com esses avanços.
+                        </p>
+                        <p className='aboutdetails'>
+                            Estou à procura de uma oportunidade para aprender, desenvolver e contribuir com o crescimento da equipe, colocando em prática o que estou estudando todos os dias.
                         </p>
                         <div className="skillsContainer">
                             <div className="skill--scroll">
@@ -43,7 +37,7 @@ function Aboutpage() {
                                     direction="left"
                                 >
                                     {skillsData.map((skill, id) => (
-                                        <div className="skill--box" key={id} >
+                                        <div className="skill--box" key={id}>
                                             <img className='skill-image' src={skillsImage(skill)} alt={skill} />
                                             <p>{skill}</p>
                                         </div>
@@ -53,21 +47,17 @@ function Aboutpage() {
                         </div>
                     </Col>
                     <Col md={5}>
-                        
                         <div className="img-hero">
-                                        <div className="rotate-text">
-                                        <div className="text">
-                                            
-                                        </div>
-                                        <span><i></i></span>
-                                        </div>
+                            <div className="rotate-text">
+                                <div className="text"></div>
+                                <span><i></i></span>
+                            </div>
                         </div>
-                        
                     </Col>
                 </Row>
             </Container>
-        </div >
-    )
+        </div>
+    );
 }
 
-export default Aboutpage
+export default Aboutpage;
